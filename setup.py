abbcaa63e58066ecf2cbb51bdfdd1405ff4739a1
r-m-n <recvmsg@protonmail.com>
2019-07-19 09:29:24 +0300

    bump version

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 
 setup(
     name='sklearn-deltatfidf',
-    version=0.2,
+    version=0.3,
     packages=['sklearn_deltatfidf'],
     description='DeltaTfidfVectorizer for scikit-learn',
     url='https://github.com/r-m-n/sklearn-deltatfidf',
-    download_url='https://github.com/r-m-n/sklearn-deltatfidf/archive/0.2.tar.gz',
+    download_url='https://github.com/r-m-n/sklearn-deltatfidf/archive/0.3.tar.gz',
     keywords=['sklearn', 'scikit-learn', 'tfidf', 'deltatfidf', 'delta tfidf'],
     license='MIT',
     install_requires=[
